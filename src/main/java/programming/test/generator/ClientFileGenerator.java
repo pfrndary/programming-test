d74a00ecec91da5b9c1ab92977b0f1a07503de4b
pfrndary <pfremaux@gmail.com>
2018-09-02 22:30:51 -0400

    adding eclipse files...

--- src/main/java/programming/test/generator/ClientFileGenerator.java
+++ src/main/java/programming/test/generator/ClientFileGenerator.java
@@ -1,19 +1,17 @@
 package programming.test.generator;
 
-import org.apache.log4j.Logger;
-import programming.test.pojo.FileInfo;
-
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamWriter;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public final class ClientFileGenerator {
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
 
-    private final static Logger logger = Logger.getLogger(ClientFileGenerator.class);
+import programming.test.pojo.FileInfo;
+
+public final class ClientFileGenerator {
 
     private static final String CLIENT_FILENAME = "client.xml";
     private static final String FILES_TAG = "FILES";
